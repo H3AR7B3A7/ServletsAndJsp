da2059838fa9950e0c2e8d29c3af572ba89cdc00
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-04 06:43:53 +0100

    :wrench: Annotated servlet edits

--- src/main/java/servlets/HelloServlet.java
+++ src/main/java/servlets/HelloServlet.java
@@ -7,17 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello", "/hi", "/ola/*"})
 public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         System.out.println("POST request was made to /hello");
-        
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         System.out.println("GET request was made to /hello");
+
+        response.setContentType("text/html");
+
+        response.getWriter().println("<h1>Hello beautiful world!</h1>");
 
     }
 }
